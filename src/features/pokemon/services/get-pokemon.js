a66406bbb3c914b7ba43aa2aa601079862e19fe9
Elynzx <rpascual06@outlook.com>
2026-04-21 14:14:20 -0500

    add getPokemonDetail function to fetch Pokémon details

---
+++ src/features/pokemon/services/get-pokemon.js
@@ -0,0 +1,8 @@
+export const getPokemonDetail = async (idOrName) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${idOrName}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("Failed getting pokemon detail");
+    }
+    return response.json();
+};
