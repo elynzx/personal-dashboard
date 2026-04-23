a8ecb3de63999c3750205a895105e6a253489837
Elynzx <rpascual06@outlook.com>
2026-04-22 19:37:39 -0500

    refactor PokemonHero component styles for improved layout and image sizing

--- src/features/pokemon/components/pokemon-hero/pokemon-hero.jsx
+++ src/features/pokemon/components/pokemon-hero/pokemon-hero.jsx
@@ -8,11 +8,11 @@ export const PokemonHero = ({ id, name, imageUrl }) => {
                 <img
                     src={imageUrl}
                     alt={name}
-                    className="w-32 h-32 md:w-30 md:h-30 object-contain relative z-20 drop-shadow-lg transition-transform hover:scale-110"
+                    className="w-28 h-28 md:w-30 md:h-30 object-contain relative z-20 drop-shadow-lg transition-transform hover:scale-110"
                 />
             </div>
-            <div className="mt-12 text-center">
-                <div className="inline-block bg-white/50 backdrop-blur-sm border border-white px-5 py-0.5 rounded-md mb-3 shadow-sm">
+            <div className="mt-4 md:mt-12 text-center">
+                <div className="inline-block bg-white/50 backdrop-blur-sm border border-white px-5 md:py-0.5 rounded-md mb-3 shadow-sm">
                     <span className="text-[10px] font-black text-bgDarkGray/60 tracking-widest">
                         POKEMON {formattedId}
                     </span>
