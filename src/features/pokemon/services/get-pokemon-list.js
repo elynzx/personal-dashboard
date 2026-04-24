f762db4edf9fac24a10dff2e2156e3b3139e1d32
Elynzx <rpascual06@outlook.com>
2026-04-24 14:36:58 -0500

    refactor getPokemonsList to use POKEMON_LIMIT constant and rename getPokemonDetail to getPokemon

--- src/features/pokemon/services/get-pokemon-list.js
+++ src/features/pokemon/services/get-pokemon-list.js
@@ -1,4 +1,6 @@
-export const getPokemonsList = async (offset = 0, limit = 10) => {
+import { POKEMON_LIMIT } from "../utils/constants";
+
+export const getPokemonsList = async (offset = 0, limit = POKEMON_LIMIT) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     const response = await fetch(url);
     if (!response.ok) {
--- src/features/pokemon/services/get-pokemon.js
+++ src/features/pokemon/services/get-pokemon.js
@@ -1,4 +1,4 @@
-export const getPokemonDetail = async (idOrName) => {
+export const getPokemon = async (idOrName) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${idOrName}`;
     const response = await fetch(url);
     if (!response.ok) {
