456d2d938c4e0d3f81044316db8c44cba922ccc6
Elynzx <rpascual06@outlook.com>
2026-04-25 11:11:52 -0500

    refactor: remove unused pokemon storage functions and integrate state management with zustand

--- src/features/pokemon/store/use-pokemon-store.js
+++ src/features/pokemon/store/use-pokemon-store.js
@@ -2,18 +2,41 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 export const usePokemonStore = create(
-    persist((set, get) => ({
-        pokemonList: [],
+    persist(
+        (set, get) => ({
+            pokemonList: [],
 
-        addPokemons: (newPokemons) => {
-            const currentPokemonList = get().pokemonList;
-            const newPokemonsToAdd = newPokemons.filter(
-                (newPokemon) => !currentPokemonList.some((pokemonInList) => pokemonInList.id === newPokemon.id),
-            );
-            if (newPokemonsToAdd.length > 0) {
-                set({ pokemonList: [...currentPokemonList, ...newPokemonsToAdd] });
-            }
+            addPokemons: (newPokemons) => {
+                const currentPokemonList = get().pokemonList;
+                const newPokemonsToAdd = newPokemons.filter(
+                    (newPokemon) =>
+                        !currentPokemonList.some(
+                            (pokemonInList) =>
+                                pokemonInList.id === newPokemon.id,
+                        ),
+                );
+                if (newPokemonsToAdd.length > 0) {
+                    set({
+                        pokemonList: [
+                            ...currentPokemonList,
+                            ...newPokemonsToAdd,
+                        ],
+                    });
+                }
+            },
+
+            getPokemons: (idOrName) => {
+                return (
+                    get().pokemonList.find(
+                        (pokemon) =>
+                            pokemon.name === idOrName ||
+                            pokemon.id === Number(idOrName),
+                    ) || null
+                );
+            },
+        }),
+        {
+            name: "pokemon-store",
         },
-        
-    })),
+    ),
 );
