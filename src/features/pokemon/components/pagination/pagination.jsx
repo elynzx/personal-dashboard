6bf7447fcd2b72e3f7c9688e7f1de99199e208f5
Elynzx <rpascual06@outlook.com>
2026-04-25 12:50:39 -0500

    refactor: update Pagination component layout and integrate 'firstPage' button

--- src/features/pokemon/components/pagination/pagination.jsx
+++ src/features/pokemon/components/pagination/pagination.jsx
@@ -1,26 +1,49 @@
-import { ArrowRightBox, ArrowLeftBox } from "pixelarticons/react";
+import {
+    ChevronLeft2,
+    SquareChevronLeft,
+    SquareChevronRight,
+} from "pixelarticons/react";
 
-export const Pagination = ({ currentPage, onPrevPage, onNextPage }) => {
+export const Pagination = ({
+    currentPage,
+    firstPage,
+    onPrevPage,
+    onNextPage,
+}) => {
     return (
-        <div className="flex justify-center text-white items-center py-4 gap-4 bg-bgDarkGray w-full mt-auto rounded-b-xl shadow-inner">
+        <div className="flex justify-between text-white tracking-wide font-item text-[12px] items-center py-6 gap-4 px-12 bg-bgDarkGray w-full mt-auto rounded-b-xl">
             <button
-                className="disabled:opacity-30 hover:scale-110 transition-transform cursor-pointer"
-                onClick={onPrevPage}
+                className="text-white flex items-center justify-center uppercase disabled:opacity-30 hover:text-bgDarkPink hover:scale-110 transition-transform"
+                onClick={firstPage}
                 disabled={currentPage === 1}
             >
-                <ArrowLeftBox />
+                <ChevronLeft2 size={24} />
+                First
             </button>
-            <div className="bg-white/10 px-4 rounded-md  font-item">
-                <span className="font-bold text-xs uppercase tracking-widest">
-                    Page {currentPage}
-                </span>
+            <div className="flex gap-4">
+                <button
+                    className="disabled:opacity-30 hover:text-bgDarkPink hover:scale-110 transition-transform cursor-pointer"
+                    onClick={onPrevPage}
+                    disabled={currentPage === 1}
+                >
+                    <SquareChevronLeft />
+                </button>
+                <div className="bg-white/10 px-4 py-0.5 rounded-md select-none flex items-center justify-center">
+                    <span className="font-semibold uppercase">
+                        Page {currentPage}
+                    </span>
+                </div>
+                <button
+                    className="hover:text-bgDarkPink hover:scale-110 cursor-pointer"
+                    onClick={onNextPage}
+                >
+                    <SquareChevronRight />
+                </button>
             </div>
-            <button
-                className="hover:scale-110 transition-transform cursor-pointer"
-                onClick={onNextPage}
-            >
-                <ArrowRightBox />
-            </button>
+            <div className="flex items-center gap-1 opacity-0 hidden sm:flex">
+                <ChevronLeft2 size={24} />
+                <span className=" uppercase font-mono">Next</span>
+            </div>
         </div>
     );
 };
