cd9b2db32c8a51d3d7b1e303be773e859e7a56da
Elynzx <rpascual06@outlook.com>
2026-04-25 11:11:33 -0500

    feat: add createPokemonProfile utility function for pokemon data structuring

---
+++ src/features/pokemon/utils/pokemon-helpers.js
@@ -0,0 +1,11 @@
+export const createPokemonProfile = (pokemon) => ({
+    id: pokemon.id,
+    name: pokemon.name,
+    image:
+        pokemon.sprites?.versions?.["generation-v"]?.["black-white"]?.animated
+            ?.front_default || pokemon.sprites?.front_default,
+    weight: pokemon.weight,
+    height: pokemon.height,
+    types: pokemon.types?.map((type) => type.type.name),
+    abilities: pokemon.abilities?.map((ability) => ability.ability.name),
+});
