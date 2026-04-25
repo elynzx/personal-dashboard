0bb8e6f988ffbe74caae0706548b4d38cf2270c0
Elynzx <rpascual06@outlook.com>
2026-04-25 15:41:40 -0500

    refactor: add skeleton components to loading state in PokemonPage

--- src/features/pokemon/pages/pokemon.jsx
+++ src/features/pokemon/pages/pokemon.jsx
@@ -4,12 +4,15 @@ import { ArrowRightBox, ArrowLeftBox } from "pixelarticons/react";
 import { PokemonCard } from "../components/pokemon-card/pokemon-card";
 import { Pagination } from "../components/pagination/pagination";
 import { usePaginationStore } from "../store/use-pagination-store";
+import { PokemonSkeleton } from "../components/skeletons/pokemon-skeleton";
 
 export function PokemonPage() {
     const page = usePaginationStore((state) => state.page);
     const limit = usePaginationStore((state) => state.limit);
     const setPage = usePaginationStore((state) => state.setPage);
-    const resetPagination = usePaginationStore((state) => state.resetPagination);
+    const resetPagination = usePaginationStore(
+        (state) => state.resetPagination,
+    );
 
     const offset = (page - 1) * limit;
     const { data, loading, error } = useGetPokemons(offset, limit);
@@ -17,19 +20,24 @@ export function PokemonPage() {
     const handleNextPage = () => setPage(page + 1);
     const resetPage = () => resetPagination();
 
-    if (loading) {
+    if (error) {
         return (
             <div className="flex flex-col flex-1 items-center justify-center w-full bg-linear-to-b from-bgPink to-bgGreen rounded-t-xl">
                 <h2 className="text-center font-item font-bold text-bgDarkGray animate-pulse">
-                    Loading...
+                    An error ocurred.
                 </h2>
             </div>
         );
     }
+
+    if (loading) {
+        return <PokemonSkeleton />;
+    }
+
     return (
-        <>
-            <div className="flex-1 overflow-y-auto scrollbar-hide w-full bg-linear-to-b from-bgPink to-bgGreen rounded-t-xl py-8 md:py-12 md:px-18">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 py-8 md:mt-4 gap-3 justify-items-center items-center min-h-full">
+        <div className="flex flex-col h-full w-full bg-linear-to-b from-bgPink to-bgGreen rounded-t-xl overflow-hidden">
+            <div className="flex-1 overflow-y-auto scrollbar-hide flex flex-col items-center justify-center p-6 md:p-12">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 justify-items-center w-full max-w-6xl">
                     {data?.map((pokemon) => (
                         <PokemonCard key={pokemon.name} pokemon={pokemon} />
                     ))}
@@ -42,6 +50,6 @@ export function PokemonPage() {
                 onPrevPage={handlePrevPage}
                 onNextPage={handleNextPage}
             />
-        </>
+        </div>
     );
 }
