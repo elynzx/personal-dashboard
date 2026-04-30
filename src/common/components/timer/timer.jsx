9144365aa587266cfe61cea33b5525d0de2b9346
Elynzx <rpascual06@outlook.com>
2026-04-29 23:49:15 -0500

    feat: add date-fns for date formatting in Timer component

--- src/common/components/timer/timer.jsx
+++ src/common/components/timer/timer.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { format } from "date-fns";
 
 export const Timer = () => {
     const [date, setDate] = useState(new Date());
@@ -10,26 +11,15 @@ export const Timer = () => {
         return () => clearInterval(timer);
     }, []);
 
-    const timeString = date.toLocaleTimeString("en-US", {
-        hour12: false,
-        hour: "2-digit",
-        minute: "2-digit",
-    });
-
-    const ampm = date
-        .toLocaleTimeString("en-US", {
-            hour12: true,
-            hour: "numeric",
-        })
-        .split(" ")[1];
-
-    const day = date.toLocaleDateString("en-US", { weekday: "long" });
+    const timeString = format(date, "HH:mm");
+    const ampm = format(date, "aa");
+    const day = format(date, "eeee");
 
     return (
-        <div className="hidden lg:flex py-3 w-52 text-bgDarkGray/80 font-bold rounded-3xl bg-white border border-bgDarkGray flex-col items-center justify-center">
+        <div className="hidden bg-white lg:flex py-3 w-52 text-bgDarkGray/80 font-bold rounded-3xl border border-dotted border-bgDarkGray flex-col items-center justify-center">
             <span className="font-title">Be kind</span>
             <span className="text-4xl">{timeString}</span>
-            <span className="font-bold">{ampm}</span>
+            <span className="font-bold uppercase">{ampm}</span>
             <span className="capitalize">{day}</span>
         </div>
     );
